386a27949ab11673fb6ce04fee8bd21658101924
shainzuo <82454790+shainzuo@users.noreply.github.com>
2023-12-24 21:02:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import SearchResult from './components/SearchResult';
+import fetchSheetData from './services/sheetBestService';
 import './App.css';
 
 function App() {
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = async (searchTerm) => {
+    const data = await fetchSheetData(searchTerm);
+    setSearchResult(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Record Search App</h1>
+      <SearchBar onSearch={handleSearch} />
+      <SearchResult data={searchResult} />
     </div>
   );
 }
